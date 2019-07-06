2bfebb67c3ca1af47bd7b6d8c820027ae83f9b97
wupeiwen <javapeiwen2010@gmail.com>
2019-07-06 10:05:01 +0800

    fix: 修复heatmap数据无法更新的问题

--- src/components/config/heatmap.js
+++ src/components/config/heatmap.js
@@ -2,14 +2,10 @@ import Base from './base'
 export default class Heatmap extends Base {
   constructor (osm, heatmap) {
     super(osm)
+    let data = this.setFeatures(heatmap.data)
     this.config.sources['heatmap-data'] = {
       'type': 'geojson',
-      'data': {
-        'type': 'FeatureCollection',
-        'features': heatmap.data.map(item => {
-          return { 'type': 'Feature', 'properties': item, 'geometry': { 'type': 'Point', 'coordinates': [ item.lng, item.lat ] } }
-        })
-      }
+      'data': data
     }
     this.config.layers.push({
       'id': 'heat',
@@ -24,5 +20,15 @@ export default class Heatmap extends Base {
         'heatmap-color': heatmap.color || ['interpolate', ['linear'], ['heatmap-density'], 0, 'rgba(0, 0, 255, 0)', 0.1, 'royalblue', 0.3, 'cyan', 0.5, 'lime', 0.7, 'yellow', 1, 'red']
       }
     })
+  }
+
+  setFeatures (data) {
+    let features = {
+      'type': 'FeatureCollection',
+      'features': data.map(item => {
+        return { 'type': 'Feature', 'properties': item, 'geometry': { 'type': 'Point', 'coordinates': [ item.lng, item.lat ] } }
+      })
+    }
+    return features
   }
 }
